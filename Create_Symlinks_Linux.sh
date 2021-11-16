caa9decfa0cfa05331ee45fc31567e5e3af51dda
techrisdev <techrisdev>
2021-11-16 23:09:10 +0100

    Fixed alacritty

--- Create_Symlinks_Linux.sh
+++ Create_Symlinks_Linux.sh
@@ -2,9 +2,13 @@
 rm -f ~/.zshrc
 rm -f ~/.zshrcmac
 rm -f ~/.zshrclinux
+rm -rf ~/.config/alacritty
 cd ~/.dotfiles
 stow dotfiles
 cd
 rm -rf ~/Library
 rm -f .zshrc
 mv .zshrclinux .zshrc
+cd ~/.config/alacritty
+rm -f alacritty.yml
+mv alacrittylinux.yml alacritty.yml
